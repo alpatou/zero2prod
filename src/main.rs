191f9fb5d5a73d906c1f64b31e9fd9774c7f84bc
Your Name <you@example.com>
2024-07-02 01:18:33 +0300

    bin and lib

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name: &str = req.match_info().get("name").unwrap_or("default");
-
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    return HttpServer::new(|| App::new().route("/{health_check}", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await;
+    run().await
 }

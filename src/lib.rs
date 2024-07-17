353f59ae3c022e1fbdc8af398a5e1e35c3c999dc
Your Name <you@example.com>
2024-07-17 23:49:39 +0300

    ddd

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 
 async fn health_check() -> HttpResponse {
@@ -7,9 +9,9 @@ async fn health_check() -> HttpResponse {
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run(address: &str) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind(address)?
+        .listen(listener)?
         .run();
 
     Ok(server)
